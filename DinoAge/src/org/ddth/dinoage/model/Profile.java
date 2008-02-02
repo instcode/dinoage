327d5e7aaed13dde3fc5b050842706f307ee17bd
instcode <instcode@9ef17325-2b44-0410-8707-2da27c8fe9a0>
2008-02-02 16:39:14 +0000

    Re-factored: Move the State interface out of Profile

--- DinoAge/src/org/ddth/dinoage/model/Profile.java
+++ DinoAge/src/org/ddth/dinoage/model/Profile.java
@@ -7,145 +7,80 @@
  **************************************************/
 package org.ddth.dinoage.model;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.ddth.dinoage.ResourceManager;
-import org.ddth.dinoage.grabber.yahoo.ContentHandlerFactory;
-import org.ddth.grabber.core.connection.Session;
-import org.ddth.grabber.core.connection.State;
-import org.ddth.grabber.core.handler.NavigationHandler;
 
-public class Profile implements State {
+public class Profile {
 	private static final String BACKUP_URLS_OUTGOING = "backup.urls.outgoing";
-	private static final String BACKUP_GUESTBOOK_INDEX = "backup.guestbook.index";
-	private static final String BACKUP_ENTRY_INDEX = "backup.entry.index";
 	private static final String BACKUP_GUESTBOOK_ENABLE = "backup.guestbook.enable";
 	private static final String BACKUP_ENTRY_ENABLE = "backup.entry.enable";
 	private static final String PROFILE_URL = "profile.url";
 	private static final String PROFILE_NAME = "profile.name";
 	private static final String BACKUP_URLS_COMPLETED = "backup.urls.completed";
-
-	private Log logger = LogFactory.getLog(Profile.class);
 	
-	private Map<String, Boolean> completedURLMap = new ConcurrentHashMap<String, Boolean>();
-	private Map<String, NavigationHandler> outgoingURLMap = new ConcurrentHashMap<String, NavigationHandler>();
-	private String profileId;
 	private String profileURL;
 	private String profileName;
 	private boolean isBackupGuestbook;
 	private boolean isBackupEntry;
-	private Persistence persistence;
+	private String outgoingURLs;
+	private String completedURLs;
 
-	public void initialize(Persistence persistence) {
-		this.persistence = persistence;
-	}
-	
-	public static String getProfileId(String profileURL) {
-		int begin = ResourceManager.KEY_PROFILE_URL.length();
-		int end = profileURL.indexOf("?", begin);
-		end = (end == -1) ? profileURL.length() : end;
-		return profileURL.substring(begin, end);
-	}
-	
-	public void load(File profileFile, Session session) throws IOException {
-		File resumeFile = new File(profileFile, ResourceManager.RESUME_FILE_NAME);
+	public void load(File profileFile) throws IOException {
+		InputStream inputStream = new FileInputStream(profileFile);
+		
 		Properties properties = new Properties();
-		properties.load(new FileInputStream(resumeFile));
-		setProfile(properties.getProperty(PROFILE_NAME, ""));
+		properties.load(inputStream);
+		
+		setProfileName(properties.getProperty(PROFILE_NAME, ""));
 		setProfileURL(properties.getProperty(PROFILE_URL, ""));
 		setBackupEntry(Boolean.parseBoolean(properties.getProperty(BACKUP_ENTRY_ENABLE, "false")));
 		setBackupGuestbook(Boolean.parseBoolean(properties.getProperty(BACKUP_GUESTBOOK_ENABLE, "false")));
-		
-		int entry = Integer.parseInt(properties.getProperty(BACKUP_ENTRY_INDEX, "0"));
-		int guestbook = Integer.parseInt(properties.getProperty(BACKUP_GUESTBOOK_INDEX, "0"));
-		initialize(new Persistence(resumeFile, new int[] {entry, guestbook}));
-		
-		String sCompletedURLs = properties.getProperty(BACKUP_URLS_COMPLETED, "");
-		String[] completedURLs = sCompletedURLs.split(",");
-		for (String completedURL : completedURLs) {
-			completedURLMap.put(completedURL, Boolean.TRUE);
-		}
-		
-		String sOutgoingURLs = properties.getProperty(BACKUP_URLS_OUTGOING, "");
-		String[] outgoingURLs = sOutgoingURLs.split(",");
-		
-		for (String outgoingURL : outgoingURLs) {
-			NavigationHandler contentHandler = ContentHandlerFactory.getInstance().createContentHandler(
-					outgoingURL, persistence, session);
-			if (contentHandler != null) {
-				outgoingURLMap.put(outgoingURL, contentHandler);
-			}
-			else {
-				logger.error(ResourceManager.getMessage(
-							"Cannot create handler for outgoing {0} url. Resume information might be corrupt.",
-								new String[] {outgoingURL}));
-			}
-		}
+		setCompletedURLs(properties.getProperty(BACKUP_URLS_COMPLETED, ""));
+		setOutgoingURLs(properties.getProperty(BACKUP_URLS_OUTGOING, ""));
 	}
 
-	public void store(OutputStream outputStream) throws IOException {
-		Writer writer = new BufferedWriter(new OutputStreamWriter(outputStream));
-		writer.write(ResourceManager.getMessage(
+	public void store(File profileFile) throws IOException {
+		OutputStream outputStream = new FileOutputStream(profileFile);
+		
+		Properties properties = new Properties();
+		properties.put(PROFILE_NAME, profileName);
+		properties.put(PROFILE_URL, profileURL);
+		properties.put(BACKUP_ENTRY_ENABLE, isBackupEntry);
+		properties.put(BACKUP_GUESTBOOK_ENABLE, isBackupGuestbook);
+		properties.put(BACKUP_URLS_COMPLETED, completedURLs);
+		properties.put(BACKUP_URLS_OUTGOING, outgoingURLs);
+		
+		properties.store(outputStream, ResourceManager.getMessage(
 				ResourceManager.KEY_PROFILE_RESUME_FILE_HEADER, new String [] {profileName, profileURL}));
-
-		writer.write("\n# Profile information\n");
-		writer.write(PROFILE_NAME + "=" + profileName + "\n");
-		writer.write(PROFILE_URL + "=" + profileURL + "\n");
-		writer.write("\n# State information\n");
-		writer.write(BACKUP_ENTRY_ENABLE + "=" + isBackupEntry + "\n");
-		writer.write(BACKUP_ENTRY_INDEX + "=" + persistence.getCategoryIndex(Persistence.BLOG_ENTRY) + "\n");
-		writer.write(BACKUP_GUESTBOOK_ENABLE + "=" + isBackupGuestbook + "\n");
-		writer.write(BACKUP_GUESTBOOK_INDEX + "=" + persistence.getCategoryIndex(Persistence.GUESTBOOK) + "\n");
-		writer.write("\n# Completed URLs\n");
-		writer.write(BACKUP_URLS_COMPLETED + "=\\\n");
-		Iterator<String> completedURLs = completedURLMap.keySet().iterator();
-		while (completedURLs.hasNext()) {
-			String sURL = completedURLs.next();
-			// Only "real" completed URL
-			if (completedURLMap.get(sURL).booleanValue()) {
-				writer.write("\t" + sURL + (completedURLs.hasNext() ? ",\\\n" : "\n"));
-			}
-		}
-		writer.write("\n# Outgoing URLs\n");
-		writer.write(BACKUP_URLS_OUTGOING + "=\\\n");
-		Iterator<String> outgoingURLs = outgoingURLMap.keySet().iterator();
-		while (outgoingURLs.hasNext()) {
-			String sURL = outgoingURLs.next();
-			writer.write("\t" + sURL + (outgoingURLs.hasNext() ? ",\\\n" : "\n"));
-		}
-		writer.flush();
 	}
 
-	public Map<String, Boolean> getCompletedMap() {
-		return completedURLMap;
-	}
-
-	public Map<String, NavigationHandler> getOutgoingMap() {
-		return outgoingURLMap;
-	}
-
-	public void setProfile(String profile) {
-		this.profileName = profile;
-	}
-
-	public String getProfile() {
+	public String getProfileName() {
 		return profileName;
 	}
 
-	public boolean isNewlyCreated() {
-		return (persistence == null);
+	public void setProfileName(String profile) {
+		if (profile == null || profile.trim().length() == 0) {
+			throw new IllegalArgumentException("Profile cannot be empty!");
+		}
+		this.profileName = profile;
+	}
+
+	public String getProfileURL() {
+		return profileURL;
+	}
+
+	public void setProfileURL(String profileURL) {
+		if (profileURL == null || profileURL.trim().length() == 0) {
+			throw new IllegalArgumentException("ProfileURL cannot be empty!");
+		}
+		this.profileURL = profileURL;
 	}
 
 	public void setBackupEntry(boolean isBackupEntry) {
@@ -164,16 +99,19 @@ public class Profile implements State {
 		return isBackupGuestbook;
 	}
 
-	public String getProfileURL() {
-		return profileURL;
+	public String getOutgoingURLs() {
+		return outgoingURLs;
 	}
-
-	public void setProfileURL(String profileURL) {
-		this.profileURL = profileURL;
-		this.profileId = getProfileId(profileURL);
+	
+	public void setOutgoingURLs(String sOutgoingURLs) {
+		this.outgoingURLs = sOutgoingURLs;
 	}
-
-	public String getProfileId() {
-		return profileId;
+	
+	public String getCompletedURLs() {
+		return completedURLs;
+	}
+	
+	public void setCompletedURLs(String sCompletedURLs) {
+		this.completedURLs = sCompletedURLs;
 	}
 }
