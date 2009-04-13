8a283454d17452f7e39e5041ce16e00a29e2838e
instcode <instcode@9ef17325-2b44-0410-8707-2da27c8fe9a0>
2009-04-13 17:33:59 +0000

    Added more meaningful error message in Workspace Chooser Dialog

--- DinoAge/src/org/ddth/dinoage/ResourceManager.java
+++ DinoAge/src/org/ddth/dinoage/ResourceManager.java
@@ -31,6 +31,8 @@ public class ResourceManager {
 		"Plz stop the current backup for {0} first.";
 	public static final String KEY_MESSAGE_WORKSPACE_MUST_BE_EXISTED =
 		"Workspace folder must be existed.";
+	public static final String KEY_MESSAGE_WORKSPACE_IS_BEING_USED =
+		"Workspace is being used.";
 	
 	public static final String KEY_MESSAGE_FULL_HREF = "<a href=\"{0}\">{1}</a>";
 	public static final String KEY_MESSAGE_READY_HREF = "<a href=\"{0}\">I''m ready! Click here to visit my homepage!...</a>";
