ac182bdcf0a31c306712becc9813a73c336ee0b6
instcode <instcode@9ef17325-2b44-0410-8707-2da27c8fe9a0>
2009-05-04 17:15:00 +0000

    Support saving output data to different files if they're existed.

--- DinoAge/src/org/ddth/dinoage/model/Persistence.java
+++ DinoAge/src/org/ddth/dinoage/model/Persistence.java
@@ -7,13 +7,20 @@
  **************************************************/
 package org.ddth.dinoage.model;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ddth.http.impl.connection.ThreadPoolConnectionModel;
+
 public class Persistence {
+	private static final Log logger = LogFactory.getLog(ThreadPoolConnectionModel.class);
+	
 	private File[] folders;
 	private String[] categories;
 	
@@ -26,10 +33,21 @@ public class Persistence {
 		this.categories = categories;
 	}
 
+	private File generateFile(File directory, String name, String extension) {
+		File outputFile = null;
+		int index = 1;
+		outputFile = new File(directory, name + "." + extension);
+		while (outputFile.exists()) {
+			outputFile = new File(directory, name + "-" + index + "." + extension);
+			index++;
+		}
+		return outputFile;
+	}
+	
 	public void write(InputStream inputStream, int category, String tail) {
 		OutputStream outputStream = null;
 		try {
-			File outputFile = new File(folders[category], categories[category] + "-" + tail + ".html");
+			File outputFile = generateFile(folders[category], categories[category] + "-" + tail, "html");
 			outputStream = new FileOutputStream(outputFile);
 			byte[] buffer = new byte[4096];
 			int bytesread = 0;
@@ -42,7 +60,7 @@ public class Persistence {
 			while (bytesread > 0);
 		}
 		catch (IOException e) {
-			e.printStackTrace();
+			logger.error("Error", e);
 		}
 		finally {
 			if (outputStream != null) {
@@ -50,8 +68,27 @@ public class Persistence {
 					outputStream.close();
 				}
 				catch (IOException e) {
+					logger.error("Error", e);
 				}
 			}
 		}
+	}
+	
+	public byte[] read(InputStream inputStream) {
+		ByteArrayOutputStream savedBytes = new ByteArrayOutputStream(64000);
+		try {
+			byte[] buffer = new byte[4096];
+			int bytesread = 0;
+			do {
+				bytesread = inputStream.read(buffer, 0, 4096);
+				if (bytesread > 0) {
+					savedBytes.write(buffer, 0, bytesread);
+				}
+			} while (bytesread > 0);
+		}
+		catch (IOException e) {
+			logger.error("Error", e);
+		}
+		return savedBytes.toByteArray();
 	}
 }
