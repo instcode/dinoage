33244643e85bb6e23ad8e477439c41d98c8856d8
instcode <instcode@9ef17325-2b44-0410-8707-2da27c8fe9a0>
2009-05-31 17:34:22 +0000

    Add database storage supported. Blogs, entries, comments now could be stored/loaded from database.

---
+++ DinoAge/src/org/ddth/dinoage/data/exception/QueryDataException.java
@@ -0,0 +1,22 @@
+/****************************************************
+ * $Project: DinoAge
+ * $Date:: Mar 21, 2008
+ * $Revision: 
+ * $Author:: khoanguyen
+ * $Comment::
+ **************************************************/
+package org.ddth.dinoage.data.exception;
+
+/**
+ * @author khoa.nguyen
+ *
+ */
+public class QueryDataException extends Exception {
+
+	public QueryDataException(Exception e) {
+		super(e);
+	}
+
+	public QueryDataException() {
+	}
+}
