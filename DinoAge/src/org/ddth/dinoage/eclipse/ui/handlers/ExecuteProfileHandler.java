b4d16c401061295d29ef41767a49336ed955c51f
instcode <instcode@9ef17325-2b44-0410-8707-2da27c8fe9a0>
2009-06-27 14:32:40 +0000

    Fixed update session running status

--- DinoAge/src/org/ddth/dinoage/eclipse/ui/handlers/ExecuteProfileHandler.java
+++ DinoAge/src/org/ddth/dinoage/eclipse/ui/handlers/ExecuteProfileHandler.java
@@ -10,6 +10,7 @@ import org.ddth.dinoage.eclipse.Activator;
 import org.ddth.dinoage.eclipse.ui.UniversalUtil;
 import org.ddth.dinoage.eclipse.ui.model.ProfileNode;
 import org.ddth.dinoage.eclipse.ui.views.WorkspaceView;
+import org.ddth.http.core.SessionChangeListener;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -20,7 +21,6 @@ import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.services.IEvaluationService;
 
 public class ExecuteProfileHandler extends AbstractHandler {
 
@@ -29,48 +29,48 @@ public class ExecuteProfileHandler extends AbstractHandler {
 				.getActiveWorkbenchWindow(event);
 		IWorkbenchPage activePage = workbenchWindow.getActivePage();
 		IViewPart view = activePage.findView(WorkspaceView.ID);
-		if (view != null) {
-			IStructuredSelection selection = (IStructuredSelection) view
-					.getSite().getSelectionProvider().getSelection();
-			ProfileNode node = (ProfileNode) selection.getFirstElement();
-			DinoAge dinoage = Activator.getDefault().getDinoAge();
-			Profile profile = node.getData();
-			BrowsingSession session = dinoage.createSession(profile);
-			
-			if (session.isRunning()) {
-				if (MessageDialog.openQuestion(workbenchWindow.getShell(),
-						workbenchWindow.getShell().getText(),
-						ResourceManager.getMessage(ResourceManager.KEY_CONFIRM_STOP_ACTIVE_PROFILE,
-								new Object[] {profile.getProfileName()}))) {
-					session.shutdown();
-				}
-			}
-			else {
-				int answer = SWT.YES;
-				if (!session.isRestorable()) {
-					File profileFolder = dinoage.getWorkspace().getProfileFolder(
-							profile);
-					String message = ResourceManager.getMessage(
-							ResourceManager.KEY_RESUME_RETRIEVING_CONFIRM,
-							new String[] {
-									profile.getProfileName(),
-									profileFolder.getAbsolutePath() });
-					answer = UniversalUtil.showConfirmDlg(
-							workbenchWindow.getShell(),
-							workbenchWindow.getShell().getText(),
-							message);
-				}
-				if (answer == SWT.YES) {
-					session.restore();
-				}
-				else if (answer == SWT.NO) {
-					session.start();
-				}
+		if (view == null) {
+			return null;
+		}
+		IStructuredSelection selection = (IStructuredSelection) view
+				.getSite().getSelectionProvider().getSelection();
+		ProfileNode node = (ProfileNode) selection.getFirstElement();
+		DinoAge dinoage = Activator.getDefault().getDinoAge();
+		Profile profile = node.getData();
+		BrowsingSession session = dinoage.createSession(profile);
+		session.addSessionChangeListener((SessionChangeListener) view);
+		
+		if (session.isRunning()) {
+			if (MessageDialog.openQuestion(workbenchWindow.getShell(),
+					workbenchWindow.getShell().getText(),
+					ResourceManager.getMessage(ResourceManager.KEY_CONFIRM_STOP_ACTIVE_PROFILE,
+							new Object[] {profile.getProfileName()}))) {
+				session.shutdown();
+				session.removeSessionChangeListener((SessionChangeListener) view);
 			}
 		}
-		IEvaluationService service = (IEvaluationService) workbenchWindow.getService(
-				IEvaluationService.class);
-		service.requestEvaluation("org.ddth.dinoage.viewer.running");
+		else {
+			int answer = SWT.YES;
+			if (!session.isRestorable()) {
+				File profileFolder = dinoage.getWorkspace().getProfileFolder(
+						profile);
+				String message = ResourceManager.getMessage(
+						ResourceManager.KEY_RESUME_RETRIEVING_CONFIRM,
+						new String[] {
+								profile.getProfileName(),
+								profileFolder.getAbsolutePath() });
+				answer = UniversalUtil.showConfirmDlg(
+						workbenchWindow.getShell(),
+						workbenchWindow.getShell().getText(),
+						message);
+			}
+			if (answer == SWT.YES) {
+				session.restore();
+			}
+			else if (answer == SWT.NO) {
+				session.start();
+			}
+		}
 		return null;
 	}
 }
